0fb903c9b1acc05b1d440df1479442f0271d6eb3
Mikakokeshi <mikakokeshi00124@gmail.com>
2024-06-23 12:34:22 +0900

    Checked the usage of directive / v-for / v-if / v-show

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -4,18 +4,37 @@ import { ref } from "vue";
 defineProps({
   msg: String,
 });
+
+//　バインディング
+
+// テキストバインディング v-text
+// 属性バインディング v-bind:属性名 v-bind:href='/src/sample.png'
+// イベントバインディング v-on:イベント v-on:click='handleClick' @click='handleClick'
+// 双方向バインディング const value = ref('') v-model="value"
+
 const dataTest = "これはデータです";
 
 const myMethod = () => {
   alert("myMethod");
 };
 const inputValue = ref("");
-</script>
 
-// テキストバインディング v-text // 属性バインディング v-bind:属性名
-v-bind:href='/src/sample.png' // イベントバインディング v-on:イベント
-v-on:click='handleClick' @click='handleClick' // 双方向バインディング const
-value = ref('') v-model="value"
+// ディレクティブ
+
+// v-for
+//配列
+const fruitsArray = ["apple", "banana", "orange"];
+//オブジェクト
+const fruitsObjct = [
+  { id: 0, name: "apple" },
+  { id: 1, name: "banana" },
+  { id: 2, name: "orange" },
+];
+
+// v-if = '条件' v-else
+// v-show = '条件'
+const number = 4;
+</script>
 
 <template>
   <div class="greetings">
@@ -23,6 +42,25 @@ value = ref('') v-model="value"
     <input v-bind:placeholder="dataTest" v-model="inputValue" />
     <button @click="myMethod">myMethod</button>
     <div>{{ inputValue }}</div>
+    <br />
+    <hr />
+    <br />
+    <p>配列ループ</p>
+    <ul>
+      <li v-for="fruit in fruitsArray" v-bind:key="fruit">
+        {{ fruit }}
+      </li>
+    </ul>
+
+    <p>オブジェクトループ</p>
+    <ul>
+      <li v-for="fruit in fruitsObjct" v-bind:key="fruit.id">
+        {{ fruit.name }}
+      </li>
+    </ul>
+    <p v-if="number < 3">数字は3より小さい</p>
+    <p v-else>数字は3より大きい</p>
+    <p v-show="number === 4">数字は4</p>
   </div>
 </template>
 
