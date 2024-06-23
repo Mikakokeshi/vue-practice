a820661b96ee4a379d7e9867643dd0e5461b97c1
Mikakokeshi <mikakokeshi00124@gmail.com>
2024-06-23 12:06:09 +0900

    Checked the usage of bindings

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,17 +1,28 @@
 <script setup>
+import { ref } from "vue";
+
 defineProps({
   msg: String,
 });
 const dataTest = "これはデータです";
 
-
+const myMethod = () => {
+  alert("myMethod");
+};
+const inputValue = ref("");
 </script>
+
+// テキストバインディング v-text // 属性バインディング v-bind:属性名
+v-bind:href='/src/sample.png' // イベントバインディング v-on:イベント
+v-on:click='handleClick' @click='handleClick' // 双方向バインディング const
+value = ref('') v-model="value"
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ dataTest }}</h1>
-    //v-bind:属性名
-    <input v-text="dataTest" v-bind:placeholder='dataTest'></input>
+    <input v-bind:placeholder="dataTest" v-model="inputValue" />
+    <button @click="myMethod">myMethod</button>
+    <div>{{ inputValue }}</div>
   </div>
 </template>
 
